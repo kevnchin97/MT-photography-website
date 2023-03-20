40409669dbe1d1701dcedc90477107fdd0534925
Kevin <103618966+kevnchin97@users.noreply.github.com>
2023-03-20 19:06:19 -0400

    editing aboutme

--- src/About.js
+++ src/About.js
@@ -7,7 +7,7 @@ const About = () => {
             <h2>capturing emotions</h2>
             <h2></h2>         
             <p className="aboutP">professional photographer / videographer born and raised in Toronto. </p>
-            <p>Kevin has a passion for capturing the energy and excitement of EDM events through his lens! His love for photography began at a young age, and as he grew older, he became more and more interested in capturing the unique atmosphere of music events. Over the years, Kevin honed his skills and developed his own style, which he brings to every event he shoots. Whether it's a massive festival or an intimate club show, Kevin strives to capture the essence of the music and the people who make it. Through photography, Kevin hopes to share the joy and excitement of EDM with others and help to create lasting memories for those who attend these amazing events. </p>
+            <p>Kevin has a passion for capturing the energy and excitement of EDM events! His love for photography began at a young age, and as he grew older, he became more and more interested in capturing the unique atmosphere of music events. Over the years, Kevin honed his skills and developed his own style, which he brings to every event he shoots. Whether it's a massive festival or an intimate club show, Kevin strives to capture the essence of the music and the people who make it. Through photography, Kevin hopes to share the joy and excitement of EDM with others and help to create lasting memories for those who attend these amazing events. </p>
 
             <p className="aboutP">kevin is taking on new clients and is available for travel worldwide. For more information, feel free to reach out through email <a href="mailto:midnite.tones@outlook.com">here</a>.</p>
         </div>
