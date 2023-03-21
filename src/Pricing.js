f84c4fc2f4ce86f552a6d7bf7ff8fd26f07ad8e7
Kevin <103618966+kevnchin97@users.noreply.github.com>
2023-03-20 22:15:38 -0400

    remove fine print

--- src/Pricing.js
+++ src/Pricing.js
@@ -12,11 +12,11 @@ const Pricing = () => {
             </div>
             <p> Kevin works with clients to understand their vision and provide a personalized service that captures the essence of the event and the performers. His rates are competitive, and he offer flexible packages that can be customized to suit your specific requirements. Whether you are a promoter, artist, or festival organizer, he is committed to providing a high-quality service that exceeds your expectations. Contact him today to discuss your project and learn how he can help you capture the unforgettable moments of your event.</p>
             
-            <div className="finePrint">
+            {/* <div className="finePrint">
                 <p> *Additional fees may apply </p>
                 <p> *50% deposit is required</p>
                 <p> *No refunds for cancellations</p>
-            </div>
+            </div> */}
         </div>
     )
 }
