0a35b965b566dc28d3fba73eaa3a31e2bbf1ff27
Kevin <103618966+kevnchin97@users.noreply.github.com>
2023-03-22 20:14:40 -0400

    change homepage

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ const Home = () => {
         <div className="heroText animate__animated animate__fadeInDown">
             {/* <p>midnite tones is a professional digital content creator specializing in concert photography and event videography. </p> */}
             <p>capturing the best of the night</p>
-            <h2>interested in working with me? Shoot me an <a href="mailto:midnite.tones@outlook.com">email!</a></h2>
+            <h2>i help artists elevate their brand. shoot me an <a href="mailto:midnite.tones@outlook.com">email!</a></h2>
         </div>
     )
 }
